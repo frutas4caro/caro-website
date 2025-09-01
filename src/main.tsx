180f2bc12aef6e8b9124a6be528afc4f91c28ea3
Carolyn Aquino <caquino1223@gmail.com>
2025-09-01 13:29:48 -0400

    cleaning up

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
-/// <reference types="vite-plugin-pwa/client" />
 import { registerSW } from 'virtual:pwa-register'
 
 // PWA auto-update & offline
